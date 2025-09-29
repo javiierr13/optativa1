150f30084fce2de2128da0ccd35eb7eb96177867
javiierr13 <fjavierrj6@gmail.com>
2025-09-29 12:48:26 +0200

    de 0

--- daw.task/src/main/java/com/daw/persistence/entities/enums/Estado.java
+++
@@ -1,7 +0,0 @@
-package com.daw.daw.task.persistence.entities.enums;
-
-public enum Estado {
-	
-	PENDIENTE, EN_PROCESO, COMPLETADA
-
-}
--- daw.task/src/main/java/com/daw/persistence/repositories/TareaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.daw.daw.task.persistence.repositories;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import com.daw.daw.task.persistence.entities.Tarea;
-
-public interface TareaRepository extends ListCrudRepository<Tarea, Integer> {
-
-}
